4116465be871ee6f84ed7641df421f0be11f189f
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-07 22:08:38 +0100

    Hero
    Added header with styling
    Added nav with styling and image inside of the nav with styling
    Added h1 and span styling, added h2 as a description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header class="w-full flex justify-center items-center flex-col">
+<nav className="flex justify-between items-center w-full mb-10 pt-3">
+  <img src={logo} alt="Website Logo" className="w-28 object-contain"/>
+</nav>
+
+<h1 className='head_text'>Summarise Articles with <br classname="max-md:hidden" /> 
+<span className="orange_gradient">OpenAI GPT-4</span>
+</h1>
+<h2 className='desc'>Our innovative web tool allows you to effortlessly distill lengthy articles into concise summaries with the power of OpenAI. Just paste the article link, and let Simple Summary do the work, providing you with clear and efficient summaries in seconds. Simplify your reading experience today!</h2>
+    </header>
   )
 }
 
