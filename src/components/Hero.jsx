7903472a981c6ce81dfa17403c37eb74daeb849b
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-16 18:17:08 +0100

    Demo.jsx
    Declared const for the summary query
    Added e.preventDefault to the handleSubmit function, to stop reload
    Changed newArticle declaration to a let as it was out of scope for handleSubmit
    Added array for articles to be pushed to when summarised
    Added local storage to receive any previously summarised articles
    Added a div to display previous articles
    Added a paragraph and styling for the previous articles
    Added a div to display the current article's summary, with styling and error catching
    Added clipboard functionality to copy an article link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,12 @@ import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <header class="w-full flex justify-center items-center flex-col">
-<nav className="flex justify-between items-center w-full mb-10 pt-3">
-  <img src={logo} alt="Website Logo" className="w-28 object-contain"/>
-</nav>
+    <header class="mt-10 w-full flex justify-center items-center flex-col">
 
 <h1 className="head_text">Summarise Articles with <br classname="max-md:hidden" /> 
-<span className="orange_gradient">OpenAI GPT-4</span>
+<span className="blue_gradient">OpenAI GPT-4</span>
 </h1>
-<h2 className="desc">Our innovative web tool allows you to effortlessly distill lengthy articles into concise summaries with the power of OpenAI. Just paste the article link, and let Simple Summary do the work, providing you with clear and efficient summaries in seconds. Simplify your reading experience today!</h2>
+<h2 className="desc">Our innovative web tool allows you to effortlessly distill lengthy articles into concise summaries with the power of OpenAI. Just paste the article link, and let it do the work, providing you with clear and efficient summaries in seconds. Simplify your reading experience today!</h2>
     </header>
   )
 }
