bdef58d702d0dc2ca00847e07b558abe13c9fa2c
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-16 15:51:06 +0100

    Demo.jsx
    Added input field for the article URL
    Added submit button
    Used peer-focus to allow the button to be focused automatically when the form is focused
    Declared handleSubmit variable to fetch an API request from a link being submitted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = () => {
   <img src={logo} alt="Website Logo" className="w-28 object-contain"/>
 </nav>
 
-<h1 className='head_text'>Summarise Articles with <br classname="max-md:hidden" /> 
+<h1 className="head_text">Summarise Articles with <br classname="max-md:hidden" /> 
 <span className="orange_gradient">OpenAI GPT-4</span>
 </h1>
-<h2 className='desc'>Our innovative web tool allows you to effortlessly distill lengthy articles into concise summaries with the power of OpenAI. Just paste the article link, and let Simple Summary do the work, providing you with clear and efficient summaries in seconds. Simplify your reading experience today!</h2>
+<h2 className="desc">Our innovative web tool allows you to effortlessly distill lengthy articles into concise summaries with the power of OpenAI. Just paste the article link, and let Simple Summary do the work, providing you with clear and efficient summaries in seconds. Simplify your reading experience today!</h2>
     </header>
   )
 }
