2bf24be2cb021e1d16947cf79b4c756f29d4871d
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-16 21:21:00 +0100

    title & favicon change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ const Hero = () => {
 <h2 className="desc">Our innovative web tool allows you to effortlessly distill lengthy articles into concise summaries with the power of OpenAI. Just paste the article link, and let it do the work, providing you with clear and efficient summaries in seconds. Simplify your reading experience today!</h2>
     </header>
   )
+ 
 }
 
 export default Hero
